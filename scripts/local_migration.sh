09a5625b5082ab18ad102948a8a0bce51cd5107f
adrian.koszalka <adrian.koszalka@techminers.org>
2022-11-15 22:56:42 +0100

    change local migration script to run all migrations in one run

--- scripts/local_migration.sh
+++ scripts/local_migration.sh
@@ -8,9 +8,27 @@ else
   exit 1
 fi
 
-SCHEMA="youtube_data"
+find ../migrations/changesets/* -prune -type d | while IFS= read -r d; do
+    if [[ $(basename "$d") == 'single_migrations' ]]
+    then
+      echo -e "\nSingle migrations are running."
 
-docker run --rm -v "${PWD%/*}/migrations":/liquibase/changelog liquibase/liquibase:4.17 \
-  --url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?currentSchema=$SCHEMA" \
-  --changeLogFile=../liquibase/changelog/migrations_changelog.yaml \
-  --username=$POSTGRES_USER --password=$POSTGRES_PASSWORD update
+      docker run --rm -v "${PWD%/*}/migrations":/liquibase/changelog liquibase/liquibase:4.17 \
+        --url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" \
+        --changeLogFile=../liquibase/changelog/migrations_changelog.yaml \
+        --username=$POSTGRES_USER --password=$POSTGRES_PASSWORD update
+
+    else
+      SCHEMA="$(basename "$d")"
+      echo -e "\nMigrations for schema ${SCHEMA} are running."
+      echo -e "databaseChangeLog:\n  - includeAll:\n      path: changesets/${SCHEMA}/" > ../migrations/schema_migrations_changelog.yaml
+
+      docker run --rm -v "${PWD%/*}/migrations":/liquibase/changelog liquibase/liquibase:4.17 \
+        --url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB?currentSchema=$SCHEMA" \
+        --changeLogFile=../liquibase/changelog/schema_migrations_changelog.yaml \
+        --username=$POSTGRES_USER --password=$POSTGRES_PASSWORD update
+
+      rm ../migrations/schema_migrations_changelog.yaml
+      echo -e "Successfully run migrations for schema ${SCHEMA}."
+    fi
+done
